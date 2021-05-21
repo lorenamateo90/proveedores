8ccca572a07d83f6525d04672289c5469bb6d88e
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-05-21 13:04:42 +0200

    add page to which you navigate and remove headless option

---
+++ index.js
@@ -0,0 +1,9 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto('https://web.gencat.cat/ca/inici')
+  await browser.close()
+})();
