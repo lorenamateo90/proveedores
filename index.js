ec9cd7eba4be961af43d5a84235ff17c8fc5e91e
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-05-24 13:15:50 +0200

    add search

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const { chromium } = require('playwright');
   const browser = await chromium.launch({ headless: false, slowMo: 50 });
   const context = await browser.newContext()
   const page = await context.newPage()
+
   await page.goto('https://web.gencat.cat/ca/inici')
+  await page.fill('id=cercadorOcultGoogle', 'agenda cultural');
+  await page.click('text= Cercar');
+  
   await browser.close()
 })();
